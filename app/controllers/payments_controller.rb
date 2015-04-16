32cd6586bf4c5b583544940562ed9bd50cdf153b
Anton Mur <mibus32@gmail.com>
2015-04-16 17:22:58 +0300

    robo

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,13 @@
-class PaymentsController < RobokassaController
-  def result_callback notification
-    payee = User.find notification.params['UserId']
-    payee.wallet.deposit! notification.total
-    render text: notification.success
-  end
+# class PaymentsController < RobokassaController
+#   def result_callback notification
+#     payee = User.find notification.params['UserId']
+#     payee.wallet.deposit! notification.total
+#     render text: notification.success
+#   end
 
-  def fail_callback notification
-  end  
+#   def fail_callback notification
+#   end  
 
-  def success_callback notification
-  end
-end
+#   def success_callback notification
+#   end
+# end
