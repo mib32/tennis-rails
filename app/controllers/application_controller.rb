a690f27116d05cde7bf49b755601ac5b41d4282f
Anton Mur <mibus32@gmail.com>
2015-05-12 13:17:39 +0300

    Polymorphic layout fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_params, if: :devise_controller?
-  before_action :find_parent_record, if: :nested_recource?
+  before_action :find_parent_record, if: :nested_resource?
   layout :set_layout
 
   protected
@@ -14,16 +14,17 @@ class ApplicationController < ActionController::Base
   end
 
   def set_layout
-    params[:model_name].try(:underscore) || 'application'
+    if nested_resource?
+      params[:model_name].underscore
+    end
   end
-  def nested_recource?
-    params[:model_name].present?
+  def nested_resource?
+    params[:model_name].present? && ( params[:controller] != params[:model_name].underscore.pluralize || params[:action] == 'show')
   end
 
   def find_parent_record
-    return if params[:controller] == params[:model_name].underscore.pluralize
     klass = params[:model_name].constantize
-    id_param = params[:model_name].underscore + '_id'
+    id_param = params[:action] == 'show' ? 'id' : params[:model_name].underscore + '_id'
     @record = klass.friendly.find(params[id_param])
     instance_variable_set('@' + params[:model_name].underscore, @record)
   end
