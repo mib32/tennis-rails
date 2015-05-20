36732bfdd56dd47f4e56209b9456e6603c3cf7a7
Anton Mur <mibus32@gmail.com>
2015-05-20 12:00:59 +0300

    skip csrf

--- app/controllers/dashboard/payments_controller.rb
+++ app/controllers/dashboard/payments_controller.rb
@@ -1,6 +1,8 @@
 class Dashboard::PaymentsController < DashboardController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
   def success
-    @request = current_user.wallet.deposit_requests.find(params['OrderId'])
+    @request = DepositRequest.find(params['OrderId'])
     if @resuest.present?
       @response = @request.payment_responses.new data: DepositResponseData.new(params)
       if @response.security_key == params["SecurityKey"]
