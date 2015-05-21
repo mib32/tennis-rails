0876a3453f7b737fb1e60f7b5a788b90e836ce11
Anton Mur <mibus32@gmail.com>
2015-05-21 20:21:37 +0300

    Error when no courts

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,7 @@
 module EventsHelper
   def current_user_events_path
-    dashboard_court_events_path(court_id: current_user.courts.first)
+    if current_user.courts.any?
+      dashboard_court_events_path(court_id: current_user.courts.first)
+    end
   end
 end
