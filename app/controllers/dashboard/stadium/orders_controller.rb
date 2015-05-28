60c61a3403367d6705cf6a9c9c8fb56245a0479d
Anton Mur <mibus32@gmail.com>
2015-05-28 16:02:11 +0300

    Поправил повторяющиеся заказы

--- app/controllers/dashboard/stadium/orders_controller.rb
+++ app/controllers/dashboard/stadium/orders_controller.rb
@@ -1,6 +1,6 @@
 class Dashboard::Stadium::OrdersController < DashboardController
   def index
-    @orders = Order.joins(events: { court: :stadium }).where('stadiums.id = ?', current_user.stadium.id).paid
+    @orders = Order.joins(events: { court: :stadium }).where('stadiums.id = ? and orders.user_id <> ?', current_user.stadium.id, current_user.id).paid.uniq
     # @events = current_user.stadium.events
   end
 end
