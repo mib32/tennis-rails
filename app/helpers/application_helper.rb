3838e1e8cb07c071341f826a48b36f41448ac06d
manasovd <afroprogger@gmail.com>
2015-06-24 07:56:34 +0600

    Добавил кастомные иконки на карту, рэйк-таски. Сгенерил фикстуры из БД

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,20 @@ module ApplicationHelper
   def number_to_integer_currency number
     number_to_currency number, precision: 0
   end
-  def stadium_infowindow stadium
+
+  def stadium_infowindow(stadium)
+    result = "#{stadium.name}<br>#{stadium.phone}"
     if stadium.active?
-      "#{stadium.name}<br>#{stadium.phone}<br>#{link_to 'Перейти на страницу', stadium}"
+      result += " #{link_to 'Перейти на страницу', stadium}"
+    end
+    return result
+  end
+
+  def stadium_get_category_icon(stadium)
+    if stadium.active?
+      stadium.category.slug.to_s + '-icon.png'
     elsif stadium.locked?
-      "#{stadium.name}<br>#{stadium.phone}"
+      'gray-icon.png'
     end
   end
 end
