f4e1d20eb4165c8e61acd9bdc89810680372f789
Anton Mur <mibus32@gmail.com>
2015-08-27 00:30:06 +0300

    Try another name

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -24,13 +24,13 @@ SwitchUser.setup do |config|
   # if it returns true, the request will continue,
   # else the request will be refused and returns "Permission Denied"
   # if you switch from "admin" to user, the current_user param is "admin"
-  config.controller_guard = lambda { |current_user, request| Rails.env.development? or request.subdomain == 'switch'}
+  config.controller_guard = lambda { |current_user, request| Rails.env.development? or request.subdomain == 'test'}
 
   # view_guard is a block,
   # if it returns true, the switch user select box will be shown,
   # else the select box will not be shown
   # if you switch from admin to "user", the current_user param is "user"
-  config.view_guard = lambda { |current_user, request| Rails.env.development? or request.subdomain == 'switch' }
+  config.view_guard = lambda { |current_user, request| Rails.env.development? or request.subdomain == 'test' }
 
   # redirect_path is a block, it returns which page will be redirected
   # after switching a user.
